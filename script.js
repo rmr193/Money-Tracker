96a2c6e543937fb6f4d6008fcf76f0a8d0f04d14
Rajesh Mozumder <rajeshmozumder334@gmail.com>
2024-01-10 20:13:33 +0600

    JS

---
+++ script.js
@@ -0,0 +1,45 @@
+function calculateBalance() {
+  const monthlyIncome = parseFloat(document.getElementById('monthlyIncome').value);
+  const houseRent = parseFloat(document.getElementById('houseRent').value);
+  const carRent = parseFloat(document.getElementById('carRent').value);
+  const electricBill = parseFloat(document.getElementById('electricBill').value);
+
+
+
+  const totalExpenditure = houseRent + carRent + electricBill;
+  const balance = monthlyIncome - totalExpenditure;
+
+  if (!isNaN(balance)) {
+    document.getElementById('balance').textContent = `${balance.toFixed(2)}`;
+    document.getElementById('totalExpenses').textContent = `${totalExpenditure.toFixed(2)}`;
+  } else {
+    document.getElementById('balance').textContent = 'Please enter valid numbers';
+    document.getElementById('totalExpenses').textContent = 'Please enter valid numbers';
+  }
+}
+
+function calculateRemainingBalance() {
+  const savingsPercentage = parseFloat(document.getElementById('savingsPercentage').value);
+
+  if (isNaN(savingsPercentage) || savingsPercentage < 0 || savingsPercentage > 100) {
+    document.getElementById('remainingBalance').textContent = 'Invalid savings percentage';
+    return;
+  }
+
+  const balance = parseFloat(document.getElementById('balance').textContent.replace('$', ''));
+
+  if (balance < 0) {
+    document.getElementById('remainingBalance').textContent = 'Negative value is not allowed';
+    return;
+  }
+
+  const savings = balance * (savingsPercentage / 100);
+  const remainingBalance = balance - savings;
+
+  if (!isNaN(remainingBalance)) {
+    document.getElementById('remainingBalance').textContent = `${remainingBalance.toFixed(2)}`;
+    document.getElementById('savingAmount').textContent = `${savings.toFixed(2)}`;
+  } else {
+    document.getElementById('remainingBalance').textContent = 'Please enter valid numbers';
+  }
+}
